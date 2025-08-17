1f851c5b1268f71a5b10107b7ba49a17156ea0c4
Tim <37276968+tholterhus@users.noreply.github.com>
2025-08-17 23:34:32 +0200

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+root: true,
+env: {
+es6: true,
+node: true,
+mocha: true
+},
+extends: [
+‘eslint:recommended’
+],
+parserOptions: {
+ecmaVersion: 2020,
+sourceType: ‘module’
+},
+rules: {
+‘indent’: [
+‘error’,
+4,
+{
+‘SwitchCase’: 1
+}
+],
+‘no-console’: ‘off’,
+‘no-unused-vars’: [
+‘error’,
+{
+‘ignoreRestSiblings’: true,
+‘argsIgnorePattern’: ‘^_’
+}
+],
+‘no-var’: ‘error’,
+‘no-trailing-spaces’: ‘error’,
+‘prefer-const’: ‘error’,
+‘quotes’: [
+‘error’,
+‘single’,
+{
+‘avoidEscape’: true,
+‘allowTemplateLiterals’: true
+}
+],
+‘semi’: [
+‘error’,
+‘always’
+],
+‘object-curly-spacing’: [
+‘error’,
+‘always’
+]
+}
+};
